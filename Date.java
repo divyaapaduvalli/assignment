8350ec3863270b5fe578fd4d8c49b0898dcf08a6
divyaapaduvalli <40786110+divyaapaduvalli@users.noreply.github.com>
2018-07-10 10:46:07 +0530

    Update Date.java

--- Date.java
+++ Date.java
@@ -3,45 +3,45 @@ public class Date {
     private int day;
     private int month;
     private int year;
-    Date(int d,int m,int y){
-        if(d>0&&d<31) {
-            day = d;
+    Date(int day,int month,int year){
+        if(day>0&&day<31) {
+            this.day = day;
         }
         else{
             System.out.println("invalid day");
         }
-        if(m>0&&m<12) {
-            month = m;
+        if(month>0&&month<12) {
+            this.month = month;
         }
         else{
             System.out.println("invalid month");
         }
-        if(y>0&&y<2018) {
-            year = y;
+        if(year>0&&year<2018) {
+            this.year = year;
         }
         else{
             System.out.println("invalid year");
         }
     }
     int maxDays() {
-        int md = daysInMonth[month - 1];
+        int max = daysInMonth[this.month - 1];
         // correction for Feb
-        return md;
+        return max;
     }
 
     public void addDays(int n) {
-        int d = this.day += n;
-        int m = this.month;
-        int y = this.year;
-        while (d > maxDays()) {
-            d = d - maxDays();
-            m++;
-            if (m > 12) {
-                y++;
-                m = 1;
+        int day = this.day += n;
+        int month = this.month;
+        int year = this.year;
+        while (day > maxDays()) {
+            day = day - maxDays();
+            month++;
+            if (month > 12) {
+                year++;
+                month = 1;
             }
         }
-        System.out.println(d+"/"+m+"/"+y);
+        System.out.println(date+"/"+month+"/"+year);
     }
     public static void main(String[] args){
         Date obj= new Date(04,10,1996);
